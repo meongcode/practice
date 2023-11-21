1542f4759b99d66f777fb4298a24fdfc75f4f8ef
meongcode <147586074+meongcode@users.noreply.github.com>
2023-11-21 09:26:30 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.23 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public long solution(long n)
+    {
+        for (long x = 1; x*x <= n; x++)
+        {
+            if (x*x == n)
+            {
+                return (x + 1) * (x + 1);
+            }
+        }
+        return -1; 
+    }
+}
