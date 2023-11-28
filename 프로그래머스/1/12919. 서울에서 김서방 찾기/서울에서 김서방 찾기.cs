e5d012d8ccfb95ee4764edb5e61c810cf88d1b2a
meongcode <147586074+meongcode@users.noreply.github.com>
2023-11-28 10:05:01 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.34 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public string solution(string[] seoul)
+    {
+        for (int i = 0; i < seoul.Length; i++)
+        {
+            if (seoul[i] == "Kim")
+            {
+                return "김서방은 " + i + "에 있다";
+            }
+        }
+        return "김서방 없슈";
+    }
+}
